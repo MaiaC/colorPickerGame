6bc95bd731c89afb7c7c122afe4d4c77289d271d
Maia Coghlan <maiaccog@gmail.com>
2018-09-10 19:20:19 +0800

    remove template literal

--- colorPicker.js
+++ colorPicker.js
@@ -55,7 +55,7 @@ function randomColor() {
 	var red = Math.floor(Math.random() * 256);
 	var green = Math.floor(Math.random() * 256);
 	var blue = Math.floor(Math.random() * 256);
-	var color = `rgb(${red}, ${green}, ${blue})`;
+	var color = "rgb(" + red + ", " + green + ", " + blue + ")";
 	return color;
 }
 
